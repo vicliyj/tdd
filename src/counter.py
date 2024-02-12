ff6ea2cb748fac0f20700872dc68e7aeb56cb312
vicliyj <99180260+vicliyj@users.noreply.github.com>
2024-02-12 03:01:59 -0800

    fix flake8 errors

--- src/counter.py
+++ src/counter.py
@@ -10,30 +10,27 @@ COUNTERS = {}
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
 
+
 @app.route('/counters/<name>', methods=['POST'])
-
-
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
-    if name in COUNTERS: 
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    if name in COUNTERS:
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
+
 @app.route('/counters/<name>', methods=['PUT'])
-
-
 def update_counter(name):
     currCount = COUNTERS[name]
     currCount += 1
     COUNTERS[name] = currCount
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
+
 @app.route('/counters/<name>', methods=['GET'])
-
-
 def read_counter(name):
     if name not in COUNTERS:
         COUNTERS[name] = 0
