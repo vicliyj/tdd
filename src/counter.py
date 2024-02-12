793cc42fd6ea99a8d28b545fba6f4cc362218f81
vicliyj <99180260+vicliyj@users.noreply.github.com>
2024-02-12 02:55:01 -0800

    fix counter.py

--- src/counter.py
+++ src/counter.py
@@ -12,16 +12,19 @@ COUNTERS = {}
 
 @app.route('/counters/<name>', methods=['POST'])
 
+
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
-    if name in COUNTERS:
+    if name in COUNTERS: 
         return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
 @app.route('/counters/<name>', methods=['PUT'])
+
+
 def update_counter(name):
     currCount = COUNTERS[name]
     currCount += 1
@@ -29,6 +32,8 @@ def update_counter(name):
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
 @app.route('/counters/<name>', methods=['GET'])
+
+
 def read_counter(name):
     if name not in COUNTERS:
         COUNTERS[name] = 0
